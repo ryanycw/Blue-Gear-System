47909cfccc9d6cf75a89d29e7d84633f0feabfe7
ryanycw <ryan0213.tem04@nctu.edu.tw>
2020-12-08 17:42:00 +0800

    create frontend to edit user profile

---
+++ frontend/src/Pages/Profile.js
@@ -0,0 +1,74 @@
+import React, { Component } from 'react'
+import { Redirect } from 'react-router-dom'
+import { Form, Button, Container } from 'react-bootstrap'
+const axios = require('axios')
+
+export default class Profile extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+          username: '',
+          password: '',
+          grad_class: '',
+          isSignedUp: false, // <-- initialize the signup state as false
+        };
+        this.onChange = this.onChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    onChange = (e) => this.setState({ [e.target.name]: e.target.value });
+
+    handleSubmit(event) {
+        console.log(this.state.username)
+        console.log(this.state.password)
+        axios.post(`http://127.0.0.1:8000/authentication/api/token/`,{
+            username: this.state.username,
+            password: this.state.password,
+        }).then((res) => {
+            console.log(res)
+            localStorage.setItem('token', res.data.access);
+            localStorage.setItem('user', res.config.data);
+            if (res.status === 200) {
+                this.setState({ isSignedUp: true }); // after signing up, set the state to true. This will trigger a re-render
+            }
+        }).catch(function (err){
+            console.log(err)
+        })
+
+        event.preventDefault();
+    }
+
+    render() {
+        if(!localStorage.getItem('token')){
+            return <Redirect to='login'/>
+        }
+        return (
+            <Container style={{ marginTop: '100px',}}>
+                <Form>
+                    <Form.Group controlId="formBasicUsername" style={{ width: '300px' }}>
+                        <Form.Label>Username</Form.Label>
+                        <Form.Control type="text" placeholder="Username" name="username" value={this.state.username} onChange={this.onChange}/>
+                        <Form.Text className="text-muted">
+                            
+                        </Form.Text>
+                    </Form.Group>
+
+                    <Form.Group controlId="formBasicPassword" style={{ width: '300px' }}>
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" placeholder="Password" name="password" value={this.state.password} onChange={this.onChange}/>
+                    </Form.Group>
+
+                    <Form.Group controlId="formBasicGradClass" style={{ width: '300px' }}>
+                        <Form.Label>Graduation Class</Form.Label>
+                        <Form.Control type="text" placeholder="Grad_Class" name="grad_class" value={this.state.grad_class} onChange={this.onChange}/>
+                    </Form.Group>
+
+                    <Button variant="primary" type="submit" onClick={this.handleSubmit}>
+                        Save Profile Information
+                    </Button>
+                </Form>
+            </Container>
+        )
+    }
+}
