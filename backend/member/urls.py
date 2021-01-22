39d1837bd0be82f5f7858af7791680cd2d31a513
ryanycw <ryan0213.tem04@nctu.edu.tw>
2021-01-22 11:57:39 +0800

    adding create profile api url

--- backend/member/urls.py
+++ backend/member/urls.py
@@ -1,5 +1,8 @@
-from django.conf.urls import url
-from django.urls import path, include
+from django.urls import path
+from .api import ProfileListApi, ProfileCreateApi, ProfileUpdateApi
 
 urlpatterns = [
+    path('api',ProfileListApi.as_view()),
+    path('api/create',ProfileCreateApi.as_view()),
+    path('api/<int:pk>',ProfileUpdateApi.as_view()),
 ]
